8404085cb58e5642fa93c38c649d51758d75b58f
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-25 22:43:22 -0500

    Adding solution for "Functions"

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/c-tutorial-functions/problem
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d) {
+    int arr[] = {a, b, c, d};
+    int max = a;
+    for (int i = 0; i < 4; i++) {
+        if (max < arr[i]) max = arr[i];
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
