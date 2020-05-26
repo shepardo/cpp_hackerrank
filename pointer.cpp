58675698db140c349e384bee7e8724a0d5eb6218
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-05-25 22:45:41 -0500

    Adding solution for "Pointer"

---
+++ pointer.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/c-tutorial-pointer/problem
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function
+    int sum = *a + *b;
+    int diff = *a - *b;
+    diff = diff < 0 ? diff * -1 : diff;
+    *a = sum;
+    *b = diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
