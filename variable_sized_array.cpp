bb0996564b578d6ea16c26d137c035c886c0c870
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-06-04 00:17:11 -0500

    Some fixes for variable sized array

--- variable_sized_array.cpp
+++ variable_sized_array.cpp
@@ -6,26 +6,40 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
+#include <iterator>
 using namespace std;
 
 
-const vector<string> split(string &s) {
-    vector<string> result;
-    istringsgtream is(s);
-    vector<string> result = vector<string>(istream_iterator<string>(is), istream_iterator<string>());
+vector<string> split(string &s) {
+    istringstream is(s);
+    const vector<string> result = vector<string>(istream_iterator<string>(is), istream_iterator<string>());
     return result;
+}
+
+void print_vector(const vector<string>& v) {
+    for (auto i = v.begin(); i != v.end(); i++) {
+        cout << *i << ' ';
+    }
+    cout << endl;
 }
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n, q;
-    vector<string> *a = new vector<string>();
     cin >> n >> q;
+    vector<string> *a = new vector<string>[n];
     for (int i = 0; i < n; i++) {
         string line;
-        cin >> line;
-        *a[i] = split(line);
+        getline(cin, line);
+        a[i] = split(line);
+        print_vector(a[i]);
     }
-    
+
+    for (int j = 0; j < q; j++) {
+        int x, y;
+        cin >> x >> y;
+        cout << a[x][y] << endl;
+    }
+    delete[] a;
     return 0;
 }
