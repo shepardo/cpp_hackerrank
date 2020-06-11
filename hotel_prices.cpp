51536d9471aa216327ccc95ae72c6193020f4e2d
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-06-10 22:12:30 -0500

    Final solution for "Hotel prices"

--- hotel_prices.cpp
+++ hotel_prices.cpp
@@ -9,7 +9,7 @@ public:
     HotelRoom(int bedrooms, int bathrooms) 
     : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
     
-    int get_price() {
+    virtual int get_price() {
         return 50*bedrooms_ + 100*bathrooms_;
     }
 private:
@@ -22,7 +22,7 @@ public:
     HotelApartment(int bedrooms, int bathrooms) 
     : HotelRoom(bedrooms, bathrooms) {}
 
-    int get_price() {
+    virtual int get_price() {
         return HotelRoom::get_price() + 100;
     }
 };
