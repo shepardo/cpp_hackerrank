1e39f8fc63201561eb78f9939761363684d3a3d8
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-09-02 21:39:11 -0500

    Final solution for "cpp sets".

--- cpp_set.cpp
+++ cpp_set.cpp
@@ -9,7 +9,29 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    set<int> s;
+    int q, n, x;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> n >> x;
+        switch (n) {
+            case 1:
+                s.insert(x);
+                break;
+            case 2:
+                s.erase(x);
+                break;
+            case 3:
+                set<int>::iterator it = s.find(x);
+                if (it != s.end()) {
+                    cout << "Yes" << endl;
+                } else {
+                    cout << "No" << endl;
+                }
+                break;
+        }
+    }  
     return 0;
 }
 
