77e3580a23a1acbc3334011dac6cb5edec05fe7a
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2022-04-03 01:01:01 -0700

    Adding solution for day 19.

---
+++ 30-day-challenge/day-19.cpp
@@ -0,0 +1,36 @@
+// day 19
+// https://www.hackerrank.com/challenges/30-interfaces/problem?isFullScreen=true
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                total += i;
+            }
+        }
+        return total;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator();
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
