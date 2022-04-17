6e62f7dcbeddcd80da994c31551ac92e1f2f05db
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2022-04-17 01:01:01 -0700

    First checkpoint for solution for problem 28.

---
+++ 30-day-challenge/day-28.cpp
@@ -0,0 +1,87 @@
+// Day 28
+// https://www.hackerrank.com/challenges/30-regex-patterns/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+int main()
+{
+     string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+    vector<string> names;
+
+    for (int N_itr = 0; N_itr < N; N_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        string firstName = first_multiple_input[0];
+
+        string emailID = first_multiple_input[1];
+
+        const regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+        std::smatch match;
+        if (regex_search(emailID, match, pattern)) {
+            if (match[3] == "gmail" && match[4] == "com") {
+                names.push_back(match[0]);
+            }
+        }
+    }
+
+    sort(names.begin(), names.end());
+    for (auto x : names)
+        cout << x << endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
